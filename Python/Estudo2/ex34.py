166cc4c0a788ccce40dc52cda8779c7e96ea6f41
alexrocrod <alexandrerrodrigues@ua.pt>
2021-06-17 21:21:00 +0100

    ex34 done

--- Python/Estudo2/ex34.py
+++ Python/Estudo2/ex34.py
@@ -1,8 +1,53 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.special import spence
 
 from metropolis import metropolis_Fermioes_2D
 
+# da overflow logo nas 1as iteracoes 
 
+if __name__ == '__main__':
+
+    nmax = 60 #60
+    N = 100 #100
+    nmedidas = 20000 #20000
+    nequi = 5000 #5000
+
+
+    Ts = np.linspace(3 , 300, 30)
+    nTs = len(Ts)
+
+    Emeds = np.zeros(nTs)
+    E2meds = np.zeros(nTs)
+    nkmed = np.zeros((nmax**2,nTs))
+
+    for i,T in enumerate(Ts):
+        print("Simulação",i+1,"T",T)
+        Emeds[i], E2meds[i], nkmed[:,i] = metropolis_Fermioes_2D(T,nequi,nmedidas,N,nmax)
+
+
+    Cv = (E2meds - Emeds**2) / (Ts**2)
+    z = nkmed[0,:] / (1-nkmed[0,:] )
+
+    Tt = np.linspace(3 , 300, 300)
+
+    Zt = np.exp(4*N/(np.pi*Tt))-1
+
+    Emt = - np.pi/4 * Tt**2 * spence(1+Zt)
+    Emt2 = N*Ts
+
+    Cvt1 = N * np.ones(300)
+
+    plt.figure(1)
+    plt.plot(Ts, Emeds-2*N, 'kx')
+    plt.plot(Tt, Emt, 'r-')
+    plt.plot(Ts, Emt2, 'r--')
+
+    plt.figure(2)
+    plt.plot(Ts, Cv, 'kx', Tt, Cvt1, 'r-')
+
+    plt.figure(3)
+    plt.plot(Ts, z, 'kx', Tt[1:], Zt[1:], 'r-')
+    plt.show()
 
 
--- Python/Estudo2/metropolis.py
+++ Python/Estudo2/metropolis.py
@@ -5,15 +5,15 @@ from vizinhos import *
 def metropolis_Bosoes_3D(T,nequi,nmedidas,N,nmax):
 
     #(1)
-    nk = np.zeros(nmax**3, dtype=int)
+    nk = np.zeros(nmax**3, dtype=np.int64)
     nk[0] = N     # todas no 1o estado
     E = 3*N
-    estado_particula = np.zeros(N, dtype=int)
+    estado_particula = np.zeros(N, dtype=np.int64)
 
     #(2)
     Emedio = 0
     E2medio = 0
-    nkmed = np.zeros(nmax**3, dtype=int)
+    nkmed = np.zeros(nmax**3, dtype=np.int64)
 
     lista_viz,nvs = lista_vizinhos_3D(nmax)
 
@@ -55,20 +55,19 @@ def metropolis_Bosoes_3D(T,nequi,nmedidas,N,nmax):
 
     return (Emedio/nmedidas,E2medio/nmedidas,nkmed/nmedidas)
 
-
 # Metropolis para bosoes 2D
 def metropolis_Bosoes_2D(T,nequi,nmedidas,N,nmax):
 
     #(1)
-    nk = np.zeros( nmax**2, dtype=int)
+    nk = np.zeros( nmax**2, dtype=np.int64)
     nk[0] = N     # todas no 1o estado
     E = 2*N
-    estado_particula = np.zeros(nmax**2, dtype=int)
+    estado_particula = np.zeros(nmax**2, dtype=np.int64)
 
     #(2)
     Emedio = 0
     E2medio = 0
-    nkmed = np.zeros(nmax**2, dtype=int)
+    nkmed = np.zeros(nmax**2, dtype=np.int64)
 
     lista_viz,nvs = lista_vizinhos_2D(nmax)
 
@@ -113,52 +112,71 @@ def metropolis_Bosoes_2D(T,nequi,nmedidas,N,nmax):
 # Metropolis para fermioes 2D
 def metropolis_Fermioes_2D(T,nequi,nmedidas,N,nmax):
 
-    #(1)
-    # nk = np.zeros( nmax**2, dtype=int)
-    # nk[0] = N     # todas no 1o estado
-    # E = 2*N
-    # estado_particula = np.zeros(nmax**2, dtype=int)
+    # PASSO 1
+    lv,nv=lista_vizinhos_2D(nmax)
 
-    # #(2)
-    # Emedio = 0
-    # E2medio = 0
-    # nkmed = np.zeros(nmax**2, dtype=int)
+    nk=np.zeros(nmax**2,dtype=np.int64)
+    nkmedio=nk
 
-    # lista_viz,nvs = lista_vizinhos_2D(nmax)
+    # calcular a energia de cada estado
+    estado_particula = np.zeros(N,dtype=np.int64)
+    Eestado = np.zeros(nmax**2,dtype=np.int64)
 
-    # npassos = nequi + nmedidas
+    for nx in range(nmax):
+        for ny in range(nmax):
+            ik = nx + nmax * ny
+            Eestado[ik] = nx**2 + ny**2
 
-    # for t in range(npassos):
-    #     for act in range(N):
-    #         # escolher uma particula
-    #         ip = np.random.randint(N)
-    #         ik = estado_particula[ip]
-    #         # escolha do vizinho
-    #         iv = np.random.randint(nvs[ik])
-    #         ikv = lista_viz[ik,iv]
-    #         #calculo da variacao da energia
-    #         # nx= ik % nmax
-    #         nx = ik % nmax +1
-    #         # ny= ik // nmax
-    #         ny = ik // nmax +1
-    #         # nxv= ikv % nmax
-    #         nxv = ikv % nmax +1
-    #         # nyv= ikv // nmax
-    #         nyv = ikv // nmax +1
+    # Eestado_ordenado, ik_ordem = np.sort(Eestado, 'ascend') # ordem crescente
+    Eestado_ordenado = np.sort(Eestado) # ordem crescente
+    ik_ordem = np.argsort(Eestado) # ordem crescente
 
-    #         dE = (nxv**2+nyv**2) - (nx**2+ny**2)
-    #         # prob de aceitacao
-    #         pA=np.minimum( 1, nvs[ik] * (nk[ikv] + 1) / (nvs[ikv] * nk[ik]) * np.exp(-dE/T))
-    #         if np.random.rand() <= pA:
-    #             E += dE
-    #             nk[ik] -= 1
-    #             nk[ikv] += 1
-    #             estado_particula[ip] = ikv
+    # colocar N particulas nos respectivos estados de forma crescente em
+    # energia
+    E = 0
+    for i in range(N):
+        estado_particula[i] = ik_ordem[i]
+        nk[ik_ordem[i]] = 1
+        E += Eestado_ordenado[i]
+
+    npassos = nequi + nmedidas
+    Emedio = 0
+    E2medio = 0
+
+    # PASSOS 2
+    for t in range(npassos):
+        for act in range(N):
+            # PASSO 2A
+            ip=np.random.randint(N)
+            ik=estado_particula[ip]
+            # PASSO 2B
+            # estado vizinho de ik escolhido ao acso de entre nv(ik)
+            ikv=lv[ik,np.random.randint(nv[ik])]
             
+            # so se fazem os calculos se o estado que se esta a considerar nao
+            # tiver ja uma particula
+            if nk[ikv] == 0:
+                # as energias ja estao calculadas por isso basta seleccionar o
+                # valor correspondente a particula escolhida aleatoriamente
+                Epi = Eestado[ik]
+                Epf = Eestado[ikv]
+                # variacao de energia
+                dE=Epf-Epi
+                
+                # PASSOS 2C
+                # probabilidade de aceitacao
+                pA=np.minimum(1, nv[ik] * (nk[ikv]+1) / (nv[ikv] * nk[ik]) * np.exp(-dE/T))
+                
+                if np.random.rand()<pA:
+                    # PASSO 2D
+                    estado_particula[ip] = ikv
+                    nk[ik] -= 1
+                    nk[ikv] += 1
+                    E += dE
+        # PASSO 2E
+        if t > nequi:
+            Emedio += E
+            E2medio += E**2
+            nkmedio += nk
 
-    #     if t > nequi:
-    #         Emedio += E
-    #         E2medio += E**2
-    #         nkmed += nk
-
-    # return (Emedio/nmedidas,E2medio/nmedidas,nkmed/nmedidas)
+    return Emedio/nmedidas,E2medio/nmedidas,nkmedio/nmedidas
